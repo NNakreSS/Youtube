86df546711e9060619cbc4848dadf7099538f0b2
NNakreSS <serkanatmaca15@gmail.com>
2024-07-31 19:53:30 +0300

    MarhHelper eklendi | Kod açıklamaları eklendi | alanlar ayrı sınıf olarak tanımlandı | animasyonlar setInterval yerine daha tutarlı olan animationFrame ile sağlandı

--- arm_wrestling/script.js
+++ arm_wrestling/script.js
@@ -1,9 +1,25 @@
 // Oyun için sabit değerleri tutan sınıf
 class Constants {
     static PI = Math.PI;
+    // alanları kapsayan iki çember arasındaki mesafe
     static CIRCLE_WIDTH = 20;
+    // bir çemberin toplam derecesi
     static CIRCLE_DEGREE = 360;
+    // alanların renkleri
     static COLORS = ["#deeb34", "#ebc934", "#eb9f34", "#eb7434", "#eb3434"];
+}
+
+// Trigonometri radyan ve derece işlemleri için yardımcı sınıf
+class MathHelper {
+    // derece olarak verilen değeri radyan olarak döner
+    static degreeToRadian(degree) {
+        return degree * (Math.PI / 180);
+    }
+
+    // radyan olarak verilen değeri derece olarak döner
+    static radianToDegree(radian) {
+        return radian * (180 / Math.PI);
+    }
 }
 
 // verilen id üzerinden canvas değerine erişip initial değişkenleri oluşturan bir sınıf
@@ -11,9 +27,12 @@ class Canvas {
     constructor(canvasId) {
         this.canvas = document.querySelector(canvasId);
         if (!this.canvas) throw new Error(`${canvasId} ile canvas bulunamadı!`);
+
         this.ctx = this.canvas.getContext('2d');
+
         this.width = this.canvas.clientWidth;
         this.height = this.canvas.clientHeight;
+
         this.centerX = this.width / 2;
         this.centerY = this.height / 2;
         this.radius = (this.height / 2) - 1;
@@ -51,7 +70,7 @@ class Circle {
         this.canvas.ctx.closePath();
     }
 
-    // Yardımcı ark çizim metodu
+    // Canvas contexte çember zizen method
     #drawArc(radius, color) {
         this.canvas.ctx.beginPath();
         this.canvas.ctx.arc(this.canvas.centerX, this.canvas.centerY, radius, 0, 2 * Constants.PI, false);
@@ -61,7 +80,7 @@ class Circle {
     }
 }
 
-// Dönen çizgiyi yöneten sınıf
+// Dönen takip çizgisini çizen ve yöneten sınıf
 class Line {
     constructor(canvas) {
         this.canvas = canvas;
@@ -70,7 +89,10 @@ class Line {
 
     // Çizgiyi çizer
     draw() {
-        const radian = this.#degreeToRadian(this.currentDegree);
+        // dereceyi radyan olarka alır
+        const radian = MathHelper.degreeToRadian(this.currentDegree);
+
+        // bir kordinat düzlemindeki çemberin istenen bir açı üzerinden yaya düşen noktası için x ve y kordinatlarını getirir
         const endX = this.canvas.centerX + this.canvas.radius * Math.cos(radian);
         const endY = this.canvas.centerY + this.canvas.radius * Math.sin(radian);
 
@@ -88,105 +110,99 @@ class Line {
         this.currentDegree = (this.currentDegree + 1) % 360;
         this.draw();
     }
-
-    // Dereceyi radyana çevirir
-    #degreeToRadian(degree) {
-        return degree * (Constants.PI / 180);
-    }
 }
 
-class Game {
-    constructor(gameCanvasId, lineCanvasId) {
-        this.currentDegree = 0;
-
-        this.gameCanvas = new Canvas(gameCanvasId);
-        this.lineCanvas = new Canvas(lineCanvasId);
-
-        this.circle = new Circle(this.gameCanvas);
-        this.line = new Line(this.lineCanvas);
+// Renkli alanları temsil eden sınıf
+class Area {
+    constructor(canvas, startAngle, endAngle, color) {
+        this.canvas = canvas;
+        this.startAngle = startAngle;
+        this.endAngle = endAngle;
+        this.color = color;
     }
 
-    start() {
-        // this.#drawCircle()
-        this.drawLine()
-        setInterval(() => {
-            this.#updateLine()
-        }, 30)
-
-        const areas = this.getRandomAreas(10)
-        areas.forEach(area => {
-            this.drawArea(area.startAngle, area.endAngle, area.color);
-        })
+    // Alanı çizer
+    draw() {
+        this.canvas.ctx.beginPath();
+        this.canvas.ctx.arc(
+            this.canvas.centerX,
+            this.canvas.centerY,
+            this.canvas.radius - (Constants.CIRCLE_WIDTH / 2),
+            this.startAngle,
+            this.endAngle,
+            false
+        );
+        this.canvas.ctx.strokeStyle = this.color;
+        this.canvas.ctx.lineWidth = Constants.CIRCLE_WIDTH;
+        this.canvas.ctx.stroke();
+        this.canvas.ctx.closePath();
     }
 
-    // #drawCircle() {
-    //     this.gameCtx.beginPath();
-    //     this.gameCtx.arc(this.centerX, this.centerY, this.radius, 0, 2 * CONSTANTS.PI, false);
-    //     this.gameCtx.strokeStyle = "black";
-    //     this.gameCtx.stroke();
-    //     this.gameCtx.closePath();
-    //     // inner circle
-    //     this.gameCtx.beginPath();
-    //     this.gameCtx.arc(this.centerX, this.centerY, this.radius - CONSTANTS.CircleWidth, 0, 2 * CONSTANTS.PI, false);
-    //     this.gameCtx.strokeStyle = "black";
-    //     this.gameCtx.stroke();
-    //     this.gameCtx.closePath();
-    //     // dot
-    //     this.gameCtx.beginPath();
-    //     this.gameCtx.arc(this.centerX, this.centerY, 5, 0, 2 * CONSTANTS.PI, false);
-    //     this.gameCtx.fill();
-    //     this.gameCtx.closePath();
-    // }
-
-    drawLine() {
-        const radian = getRadianToDegree(this.currentDegree);
-        const endX = this.centerX + this.radius * Math.cos(radian);
-        const endY = this.centerY + this.radius * Math.sin(radian);
-
-        this.lineCtx.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
-        this.lineCtx.beginPath();
-        this.lineCtx.moveTo(this.centerX, this.centerY);
-        this.lineCtx.lineTo(endX, endY)
-        this.lineCtx.strokeStyle = "black";
-        this.lineCtx.stroke();
-        this.lineCtx.closePath();
-    }
-
-    #updateLine() {
-        this.currentDegree = (this.currentDegree + 1) % 360
-        this.drawLine();
-    }
-
-    drawArea(startAngle, endAngle, color) {
-        this.gameCtx.beginPath();
-        this.gameCtx.arc(this.centerX, this.centerY, this.radius - (CONSTANTS.CircleWidth / 2), startAngle, endAngle, false);
-        this.gameCtx.strokeStyle = color;
-        this.gameCtx.lineWidth = CONSTANTS.CircleWidth
-        this.gameCtx.stroke();
-        this.gameCtx.closePath();
-    }
-
-    getRandomAreas(areaWidth = 20) {
+    // alanların verilerini çember üzerinde random bir kordinatta oluşturur
+    static getRandomAreas(canvas, areaWidth = 20, combo = 0) {
         const areas = [];
-        const areaWidthRadian = getRadianToDegree(areaWidth);
-        const randomDegree = CONSTANTS.CircleDegree * Math.random();
-        const startAngle = getRadianToDegree(randomDegree);
-        const endAngle = startAngle + areaWidthRadian
-        areas.push({ startAngle, endAngle, color: CONSTANTS.Colors[0] })
-        for (let index = 0; index < 4; index++) {
-            const previousArea = areas[index];
-            const newStartAngle = previousArea.endAngle;
-            const newEndAngle = newStartAngle + areaWidthRadian;
-            areas.push({ startAngle: newStartAngle, endAngle: newEndAngle, color: CONSTANTS.Colors[index + 1] })
+        const areaWidthRadian = MathHelper.degreeToRadian(areaWidth);
+        const randomDegree = Constants.CIRCLE_DEGREE * Math.random();
+        let startAngle = MathHelper.degreeToRadian(randomDegree);
+
+        /* 
+            * elimizdeki renk kadar , belirlediğimiz başlangıç açısından başlayarak belirttiğimiz genişlikteki yayları oluşturur.
+            * burada aldığımız ve default 0 olan combo değeri , ardarda yaptığımız kombo sayısını temsil eder , ve comboya göre oluşturulacak alanın hangi indexten başlayacağını belirler
+        */
+
+        for (let i = combo; i < Constants.COLORS.length; i++) {
+            const endAngle = startAngle + areaWidthRadian;
+            areas.push(new Area(canvas, startAngle, endAngle, Constants.COLORS[i]));
+            startAngle = endAngle;
         }
 
         return areas;
     }
 }
 
-function getRadianToDegree(degree) {
-    return degree * (CONSTANTS.PI / 180)
+class Game {
+    constructor(gameCanvasId, lineCanvasId) {
+        this.gameCanvas = new Canvas(gameCanvasId);
+        this.lineCanvas = new Canvas(lineCanvasId);
+        this.circle = new Circle(this.gameCanvas);
+        this.line = new Line(this.lineCanvas);
+        this.areas = [];
+        this.animationId = null;
+        this.lastUpdateTime = 0;
+        this.fps = 60
+        this.updateInterval = 1000 / this.fps
+    }
+
+    // oyunu başlatır
+    start() {
+        this.circle.draw();
+        this.line.draw();
+        this.areas = Area.getRandomAreas(this.gameCanvas, 13);
+        console.log(this.areas)
+        this.areas.forEach(area => area.draw());
+        this.#startAnimation();
+    }
+
+    // oyunu durdurur
+    stop() {
+        if (this.animationId) {
+            cancelAnimationFrame(this.animationId)
+        }
+    }
+
+    // Oyun boyunca çubuğun hareketini sağlayan animasyonu fps değerine bağlı kalarak her kullanıcıda senkron olarak çalıştırır.
+    #startAnimation() {
+        const updateLine = (currentTime) => {
+            if (currentTime - this.lastUpdateTime >= this.updateInterval) {
+                this.line.update();
+                this.lastUpdateTime = currentTime;
+            }
+            this.animationId = requestAnimationFrame(updateLine);
+        };
+        this.animationId = requestAnimationFrame(updateLine);
+    }
 }
 
-const game = new Game(gameCanvas, lineCanvas);
+
+const game = new Game("#game_canvas", "#line_canvas");
 game.start();
